9fc2a7f84fbd777ef9dff9424b151a6fe9d91837
Invenst PhoneFour <invenstphonefour@Invensts-MacBook-Pro-2.local>
2020-06-26 19:23:14 -0400

    Added GenericAccessProfileRole enum to BLECore-Bridging-Header.h

--- ios/BLECore-Bridging-Header.h
+++ ios/BLECore-Bridging-Header.h
@@ -2,3 +2,11 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+#import "React/RCTBridgeModule.h"
+
+
+typedef NS_ENUM(NSInteger, GenericAccessProfileRole) {
+    NIL = -1,
+    PERIPHERAL = 0,
+    CENTRAL = 1
+};
