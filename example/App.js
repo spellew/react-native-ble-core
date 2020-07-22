47ea101eaf7e14fd83f2f16be1d134083c0f4988
Invenst PhoneFour <invenstphonefour@Invensts-MacBook-Pro-2.local>
2020-07-22 13:51:30 -0400

    Commented out console logging in example App

--- example/App.js
+++ example/App.js
@@ -29,26 +29,26 @@ export default class App extends Component {
     });
 
     BLECore.startScanning([SERVICE_UUID], undefined);
-    console.log("started scanning for peripherals");
+    // console.log("started scanning for peripherals");
 
     BLECore.onPeripheralDiscovered(async (peripheral) => {
-      console.log("peripheral", peripheral);
+      // console.log("peripheral", peripheral);
       await BLECore.connectToPeripheral(peripheral);
-      console.log("connected to peripheral");
+      // console.log("connected to peripheral");
       const services = await BLECore.discoverPeripheralServices(peripheral, [SERVICE_UUID]);
-      console.log("got services", services);
+      // console.log("got services", services);
       const characteristics = await BLECore.discoverPeripheralCharacteristics(peripheral, SERVICE_UUID, [CHARACTERISTIC_UUID]);
-      console.log("got characteristics", characteristics);
+      // console.log("got characteristics", characteristics);
       const value = await BLECore.readCharacteristicValueForPeripheral(peripheral, SERVICE_UUID, CHARACTERISTIC_UUID);
-      console.log("got value", value);
+      // console.log("got value", value);
     });
 
     BLECore.onPeripheralDisconnected(peripheral => {
-      console.log("disconnected peripheral", peripheral);
+      // console.log("disconnected peripheral", peripheral);
     });
 
     const randomData = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-    console.log("randomData", randomData);
+    // console.log("randomData", randomData);
     BLECore.startAdvertising([{
       uuid: SERVICE_UUID,
       isPrimary: true,
@@ -62,15 +62,15 @@ export default class App extends Component {
     console.log("started advertising");
     
     BLECore.onCentralConnected((central) => {
-      console.log("central connected", central);
+      // console.log("central connected", central);
     });
     
     BLECore.onCentralDisconnected((central) => {
-      console.log("central disconnected", central);
+      // console.log("central disconnected", central);
     });
     
     BLECore.onReadRequestReceived(async (readRequest) => {
-      console.log("read request", readRequest);
+      // console.log("read request", readRequest);
       await BLECore.respondToReadRequest(readRequest.id, true);
     });
   }
